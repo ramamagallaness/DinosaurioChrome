b4a07585b7b3f335f4ce37dfe2a21d8802e77e60
Ramiro Magallanes <ramiromagallaness29@gmail.com>
2025-12-15 15:01:33 -0300

    Spraits ajustados png

--- lwjgl3/src/main/java/com/dinosauriojuego/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/dinosauriojuego/lwjgl3/Lwjgl3Launcher.java
@@ -32,7 +32,7 @@ public class Lwjgl3Launcher {
         configuration.setWindowedMode(1200, 720);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration.setWindowIcon("dino.png", "dino.png", "dino.png", "dino.png");
 
         //// This should improve compatibility with Windows machines with buggy OpenGL drivers, Macs
         //// with Apple Silicon that have to emulate compatibility with OpenGL anyway, and more.
